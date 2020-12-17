b379e3af30be42fc2948d10327fd02a9fac37113
samshaker1 <s.shaker87@gmail.com>
2020-12-17 14:52:56 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,11 @@ self.addEventListener('install', async event => {
 self.addEventListener('fetch', async event => {
   console.log('fetch event')
 });
+
+const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './app.js',
+  './styles.css'
+];
